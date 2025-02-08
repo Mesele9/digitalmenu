0e0268d624e441cde0a98f2a85f06dcd5f2e466e
Mesele Assefa <getmesy@gmail.com>
2025-02-08 20:55:35 +0000

    finalize the menu item comment feature

--- menu/admin.py
+++ menu/admin.py
@@ -1,6 +1,29 @@
-
+""" 
 from django.contrib import admin
 from .models import Category, MenuItem
 
 admin.site.register(Category)
 admin.site.register(MenuItem)
+ """
+
+from django.contrib import admin
+from .models import MainCategory, Category, MenuItem, Rating
+
+class CategoryInline(admin.TabularInline):
+    model = Category
+    extra = 1
+
+@admin.register(MainCategory)
+class MainCategoryAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline]
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'is_active']
+    filter_horizontal = ['categories']  # Allow selecting multiple categories
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ['menu_item', 'stars', 'guest_name', 'created_at']
+
+admin.site.register(Category)
--- menu/models.py
+++ menu/models.py
@@ -1,19 +1,38 @@
-
 from django.db import models
+
+class MainCategory(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    main_category = models.ForeignKey(MainCategory, on_delete=models.CASCADE)
     description = models.TextField(blank=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.main_category.name} - {self.name}"
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
+    description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
     image = models.ImageField(upload_to='menu_images/', blank=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+class Rating(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    stars = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+    comment = models.TextField(blank=True)
+    guest_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.menu_item} - {self.stars} stars"
